354b819493a9a769f96a3c065d44735cb698c1eb
HovxGovx <angelotrandria69@gmail.com>
2026-01-23 22:28:36 +0300

    minor changes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,7 +4,6 @@ import { CreateUserDto } from './dto/create-user.dto';
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
-    
     @Post()
     create(@Body() createUserDto: CreateUserDto) {
         return this.usersService.create(createUserDto);
