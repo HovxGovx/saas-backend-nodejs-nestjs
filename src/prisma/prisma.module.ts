e04ee88f35a6ce143d7b28502703d3df6a018e4c
HovxGovx <angelotrandria69@gmail.com>
2026-01-25 12:23:48 +0300

    minor changes

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-@Global() // rend PrismaService disponible partout sans importer
+@Global() 
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
